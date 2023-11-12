d679aa676ac576ef976ad417a25035ff18115939
Bragi1989 <130233226+Bragi1989@users.noreply.github.com>
2023-11-12 22:17:03 +0100

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,21 @@
+import React from 'react';
+import FeedbackApp from './FeedbackApp.jsx'; 
+
 export const App = () => {
   return (
     <div
       style={{
         height: '100vh',
         display: 'flex',
-        justifyContent: 'center',
+        flexDirection: 'column',
         alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
+        fontSize: 20,
+        color: '#010101',
+        padding: '20px'
       }}
     >
-      React homework template
+      <h1>Feedback App</h1>
+      <FeedbackApp />
     </div>
   );
 };
